8665bf574354638b04d67687f9ee2be0e7f97fbb
unknown <aelise17264@gmail.com>
2021-04-15 20:07:06 -0600

    added password security with bcrypt

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
-const md5 = require("md5")
+//const encrypt = require("mongoose-encryption")
+//const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
 
 const app = express()
 
@@ -37,22 +39,24 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err)
         }else{
             if(foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets")
-                    // console.log("success")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    if(result === true){
+                        res.render("secrets")
+
+                    }
+                })
+              }
             }
-        }
-      
+        })
     })
-})
+
 
 //creates new users
 app.get("/register", function(req, res){
@@ -60,18 +64,25 @@ app.get("/register", function(req, res){
 })
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else{
+                res.render("secrets")
+            }
+        })
+
     })
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
+
 
 })
 
